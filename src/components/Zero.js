fe076c3650e4a816e395eea8450527b47bd56bc9
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-14 15:24:49 +0530

    Zero page styling changed

--- src/components/Zero.js
+++ src/components/Zero.js
@@ -1,39 +1,68 @@
-import React from "react";
-import "../CSS_files/Zero.css";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Lottie from "react-lottie";
+import animationData from "../images/Octo-Hello.json";
+import { Button } from "react-bootstrap";
+import "../CSS_files/Zero1.css";
 
 function Zero() {
-  // const gifStyle = {
-  //   position: "absolute",
-  //   top: "2%",
-  //   right: "2%",
-  //   width: "15%",
-  //   maxWidth: "150px",
-  // };
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
-  /*const logoStyle = {
-    position: "absolute",
-    bottom: "2%",
-    right: "2%",
-    width: "8%",
-    maxWidth: "80px",
-  };*/
+  const [isButtonHovered, setIsButtonHovered] = useState(false);
 
-  // <img
-  //         src="./assets/Zero-page/iiit_logo.png"
-  //         alt="Logo"
-  //         style={logoStyle}
-  //       />
+  const backgroundStyle = {
+    backgroundImage:
+      "radial-gradient(circle 815px at 23.4% -21.8%, rgba(9,29,85,1) 0.2%, rgba(0,0,0,1) 100.2%)",
+    height: "100vh",
+    margin: 0,
+    padding: 0,
+    boxSizing: "border-box",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+
+  const buttonStyle = {
+    width: "150px",
+    backgroundColor: "#000000",
+    color: "#ffffff",
+    borderRadius: "10px",
+    padding: "10px",
+    margin: "10px",
+    border: "2px solid #ffffff",
+    cursor: "pointer",
+    transition: "transform 0.2s, color 0.2s",
+  };
+
+  const buttonHoverStyle = {
+    transform: "translateY(-5px)",
+    color: "#6852ff",
+  };
+
   return (
-    <div>
-      <div className="containerStyle">
-        <a href="/home">
-          <img
-            src="./assets/Zero-page/btnLetsBegin.png"
-            alt="Button"
-            className="btnStyle"
-            loading="eager"
-          />
-        </a>
+    <div style={backgroundStyle}>
+      <div className="text-center">
+        <div className="d-flex justify-content-center align-items-center mt-5">
+          <Lottie options={defaultOptions} height={450} width={450} />
+        </div>
+        <Button
+          as={Link}
+          to="/home"
+          variant="primary"
+          style={{ ...buttonStyle, ...(isButtonHovered && buttonHoverStyle) }}
+          onMouseEnter={() => setIsButtonHovered(true)}
+          onMouseLeave={() => setIsButtonHovered(false)}
+        >
+          Let's Begin
+        </Button>
       </div>
     </div>
   );
