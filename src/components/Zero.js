5f4d0817d69990139d8a611eec40d691b8f99d93
Rajnish Kumar Singh <b421037@iiit-bh.ac.in>
2024-01-14 15:25:25 +0530

    Zero1.css eliminated

--- src/components/Zero.js
+++ src/components/Zero.js
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import Lottie from "react-lottie";
 import animationData from "../images/Octo-Hello.json";
 import { Button } from "react-bootstrap";
-import "../CSS_files/Zero1.css";
 
 function Zero() {
   const defaultOptions = {
